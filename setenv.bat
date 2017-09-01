35f5cd42994ac072b4ae76535735bafda3c0daaf
Lu Wenshuo <e0015368@nus.edu.sg>
2017-09-01 13:08:44 +0800

    import RC 1.0.8

---
+++ setenv.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+set ANT_HOME=..\apache-ant-1.6.5
+set JUNIT_HOME=..\junit3.8.2
+
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+set CLASSPATH=%JUNIT_HOME%\junit.jar
